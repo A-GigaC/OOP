1cff033bf9815188e6403c6e8edc1d2bf896996f
Fedor Pivovarov <f3d0rp1v0v4r0v@yandex.ru>
2024-09-23 14:41:51 +0700

    refactor Dealer

--- Task_1_1_2/src/main/java/Dealer.java
+++ Task_1_1_2/src/main/java/Dealer.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 public class Dealer extends Participant {
     private Card closedCard;
 
-    // Constructor
+    /* Constructor */
     public Dealer(Deck deck) {
         this.deck = deck;
         this.cards = new ArrayList<>();
         this.score = 0;
     }
 
-    // Dealer peeks card from deck and keep it closed
+    /* Dealer peeks card from deck and keep it closed */
     public void getClosedCard() {
         closedCard = deck.peek();
         score += closedCard.getValue();
     }
 
-    // Dealer opens closed card
+    /* Dealer opens closed card */
     public Card openCard() {
         Card tmpForClosedCard = closedCard;
         cards.add(closedCard);
@@ -25,7 +25,7 @@ public class Dealer extends Participant {
         return tmpForClosedCard;
     }
 
-    // Prints cards and total value of Dealer
+    /* Prints cards and total value of Dealer */
     public void getStatus() {
         String status = "\tDealer cards: [";
         if (closedCard != null) {
