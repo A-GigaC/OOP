147e7c5eff4b29906e680114c500a7f43b8118cf
A-GigaC <A-GigaC@users.noreply.github.com>
2024-12-12 03:04:29 +0000

    Deploying to gh-pages from @ A-GigaC/OOP@606afe667b5ddc9c868d5eea353dee28e506b20a 🚀

---
+++ Task_1_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"BoyerMoor"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"RingedBuffer"},{"p":"org.example","l":"StatusCharIndex"}]
